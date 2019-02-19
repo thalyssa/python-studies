1462bff31e7432a3f3cb3c2b07dce36501c379d4
Yami Thalyssa <thalyssa2.0@hotmail.com>
2019-02-19 11:39:38 -0300

    Create birthday-date.py

---
+++ birthday-date.py
@@ -0,0 +1,6 @@
+#Criar um script python que leia o dia, o mês e o ano de nascimento de alguém e mostre a data formatada
+
+dia = input('Digite o dia de nascimento: ')
+mes = input('Agora digite o mês: ')
+ano = input('Agora digite o ano: ')
+print('Nascimento: ' + dia + ' de ' + mes + ' de ' + ano)
