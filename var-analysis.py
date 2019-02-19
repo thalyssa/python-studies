1c91158b927bad1c65176c90cac5c0e354c121ac
Yami Thalyssa <thalyssa2.0@hotmail.com>
2019-02-19 12:17:42 -0300

    Create var-analysis.py

---
+++ var-analysis.py
@@ -0,0 +1,14 @@
+#Análise de uma variável
+
+n = input('Valor: ')
+print(type(n))
+print(n.isnumeric())
+print(n.isalpha())
+print(n.isalnum())
+print(n.isupper())
+print(n.isdigit())
+print(n.isdecimal())
+print(n.isidentifier())
+print(n.islower())
+print(n.isprintable())
+print(n.isspace())
