6305873951218d3aec4eedc78c76fc46e11ccbe7
Yami Thalyssa <thalyssa2.0@hotmail.com>
2019-02-19 11:36:08 -0300

    Create greetings.py

---
+++ greetings.py
@@ -0,0 +1,4 @@
+#Ler o nome de uma pessoa e mostrar uma mensagem de boas-vindas de acordo com o valor digitado
+
+nome = input('Digite seu nome: ')
+print('Olá, ' + nome + '! Prazer em te conhecer!')
