b4dd635d5027b3b97add56097f0fde4efef5594c
Yami Thalyssa <thalyssa2.0@hotmail.com>
2019-02-26 22:34:12 -0300

    Create pre-and-sucessor.py

---
+++ pre-and-sucessor.py
@@ -0,0 +1,3 @@
+#Recebe um número e mostra o antecessor e o sucessor do mesmo
+var = int(input())
+print('Você digitou o número: {}, cujo antecessor é {} e o sucessor é {}'.format(var, var-1, var+1))
